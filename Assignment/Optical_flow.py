e7bc90890886bfe13089b6ecfa6820d544379b66
gigi <pna05006@naver.com>
2022-05-23 14:05:41 +0900

    Fix

--- Assignment/Optical_flow.py
+++ Assignment/Optical_flow.py
@@ -1,25 +1,8 @@
 import cv2
 import numpy as np
 
-## Load Image
-img_raw = cv2.imread(filename="night_pic.jpg", flags=cv2.IMREAD_COLOR).astype(np.float32) / 255.0
-img_ratio = img_raw.shape
-img = cv2.resize(img_raw, dsize=(int(img_ratio[1]/20), int(img_ratio[0]/20))) # 패딩 잘 됬는지 보려고 한 것
-
-## Pading Image
-img_padding = np.insert(img, 0, img[0], axis=0) # 위쪽 패딩
-img_padding = np.insert(img_padding, img_padding.shape[0], img_padding[img_padding.shape[0]-1], axis=0) # 아래쪽 패딩
-img_padding = np.insert(img_padding, 0, img_padding[:,0], axis=1) # 왼쪽 패딩
-img_padding = np.insert(img_padding, img_padding.shape[1], img_padding[:,img_padding.shape[1]-1], axis=1) # 오른쪽 패딩
-
-## Show Image
-img_padding = cv2.resize(img_padding, dsize=(img_ratio[1], img_ratio[0]))
-cv2.imshow("padding", img_padding)
-cv2.imshow("raw", img_raw)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-def Padding_gigi(img_raw, padding_size = 1, flags = 0):
+## Padding function
+def Padding(img_raw, padding_size = 1, flags = 0):
     if flags == 0 and padding_size == 1:
         img_padding = np.insert(img_raw, 0, img_raw[0], axis=0) # 위쪽 패딩
         img_padding = np.insert(img_padding, img_padding.shape[0], img_padding[img_padding.shape[0]-1], axis=0) # 아래쪽 패딩
@@ -29,7 +12,20 @@ def Padding_gigi(img_raw, padding_size = 1, flags = 0):
     elif flags == 1:
         return img_raw
 
-# ##test
-# img_test = np.array([[[1,1,1], [2,2,2], [3,3,3]],[[4,4,4], [5,5,5], [6,6,6]],[[7,7,7], [8,8,8], [9,9,9]]])
-# img_padding_test = Padding_gigi(img_test)
-# print(img_padding_test)
+## Load Image & down sampling
+A1_raw = cv2.resize(cv2.imread(filename="A1.jpg", flags=cv2.IMREAD_COLOR).astype(np.float32) / 255.0, dsize=(640, 400)) # 16:10 해상도
+A2_raw = cv2.resize(cv2.imread(filename="A2.jpg", flags=cv2.IMREAD_COLOR).astype(np.float32) / 255.0, dsize=(640, 400))
+
+## Remove letterbox & Padding
+for i in range(A1_raw.shape[0]):
+    print(np.mean(A1_raw[i]))
+    if np.mean(A1_raw[i]) > 0.01:
+        L_box = i
+        break
+A1_img = Padding(A1_raw[L_box:A1_raw.shape[0]-L_box]) # 16:9의 원본 사진 비율로 잘 잘린 후 1픽셀 패딩까지 잘 됨
+A2_img = Padding(A2_raw[L_box:A2_raw.shape[0]-L_box])
+
+## img show
+cv2.imshow("A1 & A2 RAW", np.vstack((A1_img, A2_img)))
+cv2.waitKey()
+cv2.destroyAllWindows()
